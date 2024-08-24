219400a415b1dfd18933fc016ea48c39e81284e8
Tu Nombre <tuemail@example.com>
2024-08-24 16:34:39 -0300

    f git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-// Home.tsx
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Navbar from "./components/navbar/navbar";
 import Header from "./components/header/header";
@@ -10,12 +9,24 @@ import Footer from "./components/footer/footer";
 import AuthModal from "./components/authModel/authModel";
 
 function Home() {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  // Este hook asegura que el componente se renderice completamente en el cliente.
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  // Mientras no se haya montado, no renderizará el contenido, evitando problemas de estilos y renderización.
+  if (!hasMounted) {
+    return null; // Esto evita el renderizado hasta que se confirme que el componente está montado
+  }
+
   return (
     <>
       <GlobalStyle />
       <Navbar />
-{/*       <Header />
- */}      <Information />
+      <Header />
+      <Information />
       <Footer />
       <AuthModal />
     </>
