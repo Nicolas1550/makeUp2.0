05dc34be894f2a644c501305a0bc2dadc7f3ac4f
Tu Nombre <tuemail@example.com>
2024-09-08 18:29:29 -0300

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { Providers } from "@/redux/provider";
-import { AuthInitializer } from "@/redux/authInitializer"; 
+import { AuthInitializer } from "@/redux/authInitializer";
 import "./globals.css";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 
 export const metadata = {
-  title: "Your App Title",
-  description: "Your App Description",
+  title: "💇‍♀️ Salon de Belleza - Gestión Online 💻",
+  description:
+    "Bienvenido/a al **Salon de Belleza**, una plataforma completa para gestionar servicios de peluquería, reservas en línea, productos, empleados, y mucho más. Este sistema es ideal para salones de belleza que buscan optimizar su flujo de trabajo tanto para los clientes como para los administradores.",
 };
 
 export default function RootLayout({
